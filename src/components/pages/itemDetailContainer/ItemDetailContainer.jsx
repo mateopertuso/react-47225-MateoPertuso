5c9ab0d044024a1564d3f47d550237ac9df3f29d
Mateo Pertuso <mateopertuso.dev@gmail.com>
2023-11-10 18:28:50 -0300

    elimine console.log

--- src/components/pages/itemDetailContainer/ItemDetailContainer.jsx
+++ src/components/pages/itemDetailContainer/ItemDetailContainer.jsx
@@ -1,46 +1,4 @@
-/* import { useContext, useEffect, useState } from "react";
-import { products } from "../../../productsMock";
-import { ItemDetail } from "./ItemDetail";
-import { useParams } from "react-router-dom"
-import { CartContext } from "../../../context/CartContext";
 
-const ItemDetailContainer = () => {
-  const [productSelected, setProductSelected] = useState({});
-
-  const { id } = useParams()
- 
-  const {addToCart, getQuantityById} = useContext(CartContext)
-
-  let totalQuantity = getQuantityById(+id)
-
-
-  useEffect(() => {
-    let producto = products.find((product) => product.id === +id);
-
-    const getProduct = new Promise((resolve) => {
-      resolve(producto);
-    });
-
-    getProduct
-      .then((res) => setProductSelected(res))
-      .catch((err) => console.log(err));
-  }, [id]);
-
-  const onAdd = (cantidad) => {
-    let item = {
-      ...productSelected,
-      quantity: cantidad,
-    };
-
-    addToCart( item )
-
-  };
-
-  return <ItemDetail productSelected={productSelected} onAdd={onAdd} initial={totalQuantity} />;
-};
-
-export default ItemDetailContainer;
- */
 import { useContext, useEffect, useState } from "react";
 import { ItemDetail } from "./ItemDetail";
 import { useParams } from "react-router-dom";
